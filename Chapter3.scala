95dd5975864fd66455329f47ab1e77c77110c911
Minsub Yim <msyim826@gmail.com>
2017-06-06 20:37:21 +0900

    Update Chapter3.scala

--- Chapter3.scala
+++ Chapter3.scala
@@ -36,3 +36,41 @@ def dropWhile[A](l:List[A])(f: A=>Boolean): List[A] = l match{
   case Nil => Nil
   case Cons(x,xs) => if(f(x)) dropWhile(xs)(f) else Cons(x,dropWhile(xs)(f))
 }
+
+/** EXERCISE 5: Using the same idea, implement the function setHead for
+replacing the first element of a List with a different value. **/
+def setHead[A](l:List[A], new_head:A): List[A] = l match{
+  case Nil => Cons(new_head,Nil)
+  case Cons(x,xs) => Cons(new_head,xs)
+}
+
+/** EXERCISE 6: Not everything works out so nicely. Implement a function,
+init, which returns a List consisting of all but the last element of a List. So,
+given List(1,2,3,4), init will return List(1,2,3). Why can't this
+function be implemented in constant time like tail?**/
+def init[A](l:List[A]):List[A] = l match{
+  case Nil => Nil
+  case Cons(x,Nil) => Nil
+  case Cons(x,xs) => Cons(x,init(xs))
+}
+
+/** EXERCISE 7: Can product implemented using foldRight immediately
+halt the recursion and return 0.0 if it encounters a 0.0? Why or why not? **/
+def product(l:List[Double]): Double = {
+  foldRight(l,1.0)(_*_)
+}
+// It cannot halt when 0.0 is met because foldRight is not tail-recursive.
+
+/** EXERCISE 8: See what happens when you pass Nil and Cons themselves to
+foldRight, like this: foldRight(List(1,2,3),Nil:List[Int])(Cons(_,_)) **/
+
+// foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_))
+// = Cons(1, foldRight(List(2,3), Nil:List[Int]) )
+// = Cons(1, Cons(2, foldRight( List(3), Nil:List[Int] ) ) )
+// = Cons(1, Cons(2, Cons(3, foldRight(Nil, Nil:List[Int]) ) ) )
+// = Cons(1, Cons(2, Cons(3, Nil) ) ) = List(1,2,3)
+
+/** EXERCISE 9: Compute the length of a list using foldRight. **/
+def length[A](l: List[A]): Int = {
+  foldRight(l,0)(1+_)
+}
